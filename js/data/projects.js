667d5a4d9444515df6fb673c1d593016882462b2
Omosuyi Olawole <omooladev@gmail.com>
2024-04-09 01:25:39 +0100

    added the shopcommerce project

--- js/data/projects.js
+++ js/data/projects.js
@@ -1,11 +1,33 @@
 //----------> Display 4 main projects
 export const projects = {
     main: [
+        //<----------- PROJECT 1 ---------->\\
         {
             name: "FlowNews",
             description: "An interactive news platform that provides users with up-to-date global news and offers registration for contributors .This feature enables users to share their own content and local events, ensuring continuous updates for all.",
             Links: { github: "https://github.com/omooladev/flownews", demo: "https://flownews.netlify.app" },
             technologies: ["HTML5", "CSS3", "SCSS", "ReactJS", "NodeJS", "ExpressJS", "MongoDB"],
+            thumbnail: "", //----------> Link to the image
+        },
+        //<----------- PROJECT 1 ---------->\\
+        {
+            name: "ShopCommerce",
+            description: "An e-commerce platform enabling users to browse and purchase products, with administrative capabilities for adding, editing, and deleting products",
+            Links: {
+                github: "https://github.com/omooladev/ShopCommerce",
+                demo: "https://shopcommerce.onrender.com",
+            },
+            technologies: [
+                "HTML5",
+                "CSS3",
+                "SCSS",
+                "Typescript",
+                "EJS",
+                "NodeJS",
+                "ExpressJS",
+                "MongoDB",
+                "Cloudinary",
+            ],
             thumbnail: "", //----------> Link to the image
         },
     ],
